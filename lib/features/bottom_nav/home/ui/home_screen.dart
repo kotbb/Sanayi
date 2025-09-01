12c7714e2635c73c59de5c8c2ede6062c6c07ce4
feras.Oz <ferasosama99@gmail.com>
2025-09-01 12:09:59 +0300

    - refactoring home screen

--- lib/features/bottom_nav/home/ui/home_screen.dart
+++ lib/features/bottom_nav/home/ui/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sanayi_app/core/helpers/spacing.dart';
 import 'package:sanayi_app/features/bottom_nav/home/ui/widgets/categories/categories_list_view_builder.dart';
 import 'package:sanayi_app/features/bottom_nav/home/ui/widgets/search_widget.dart';
@@ -11,41 +11,21 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xfffEBF0F4),
-        title: Text(
-          "Hey, Feras",
-          style: TextStyle(color: const Color(0xff102144)),
-        ),
-        actions: [
-          GestureDetector(
-            onTap: () {},
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Icon(
-                    Icons.notifications,
-                    color: const Color(0xff102144),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Color(0xfffEBF0F4), // لون الخلفية
+        statusBarIconBrightness:
+            Brightness.dark, // لون الأيقونات (Light = أبيض, Dark = أسود)
       ),
-      body: SingleChildScrollView(
+    );
+
+    return SafeArea(
+      child: SingleChildScrollView(
         child: Column(
           children: [
             Container(
               width: double.infinity,
-              height: MediaQuery.of(context).size.height * 0.26,
+              height: MediaQuery.of(context).size.height * 0.33,
               decoration: const BoxDecoration(
                 color: Color(0xfffEBF0F4),
                 borderRadius: BorderRadius.only(
@@ -58,6 +38,39 @@ class HomeScreen extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Hey, Feras",
+                          style: TextStyle(
+                            color: const Color(0xff102144),
+                            fontSize: 24.sp,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white,
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Icon(
+                                  Icons.notifications,
+                                  color: const Color(0xff102144),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    verticalSpace(24),
                     Text(
                       "We are here to facilitate your services ...",
                       style: TextStyle(fontSize: 16.sp),
@@ -72,6 +85,7 @@ class HomeScreen extends StatelessWidget {
             CategoriesListViewBuilder(),
             verticalSpace(42),
             ServicesListViewBuilder(),
+            verticalSpace(42),
           ],
         ),
       ),
